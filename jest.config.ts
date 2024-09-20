53d88fc8af1b36df83d8ffb9d3db2e78fb6b5cb8
Maksim Anosov <eastcoastcustoms@yandex.ru>
2024-09-20 12:38:30 +0300

    some lints changes

--- jest.config.ts
+++ jest.config.ts
@@ -182,8 +182,8 @@ const config: Config = {
       'ts-jest',
       {
         // настройки для ts-jest
-      },
-    ],
+      }
+    ]
   }
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
