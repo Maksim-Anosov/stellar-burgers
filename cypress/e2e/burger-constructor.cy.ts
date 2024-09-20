f2cffe2fb0c5332fd72a6aebce6faa8e7e7682c9
Maksim Anosov <eastcoastcustoms@yandex.ru>
2024-09-20 11:16:11 +0300

    cypress tests were done

--- cypress/e2e/burger-constructor.cy.ts
+++ cypress/e2e/burger-constructor.cy.ts
@@ -13,6 +13,11 @@ describe('BurgerConstructor', () => {
     cy.viewport(1920, 1080);
   });
 
+  afterEach(() => {
+    cy.clearLocalStorage();
+    cy.clearCookies();
+  });
+
   describe('Add ingredients to constructor', () => {
     it('add buns to constructor', () => {
       cy.get('[data-cy=bun-ingredients]').contains('Добавить').click();
@@ -31,6 +36,48 @@ describe('BurgerConstructor', () => {
     it('add sause ingredients to constructor', () => {
       cy.get('[data-cy=sauces-ingredients]').contains('Добавить').click();
       cy.get('.constructor-element').contains('Соус Spicy-X').should('exist');
+    });
+  });
+
+  describe('ingredients modal', () => {
+    it('open and close ingredients modal', () => {
+      cy.get('[data-cy=bun-ingredients] li').first().click();
+      cy.get('[id=modals]')
+        .contains('Краторная булка N-200i')
+        .should('be.visible');
+      cy.get('[id=modals]').find('button').click().should('not.exist');
+    });
+
+    it('close by overlay', () => {
+      cy.get('[data-cy=bun-ingredients] li').first().click();
+      cy.get('[id=modals]')
+        .find('div')
+        .click({ multiple: true, force: true })
+        .should('not.exist');
+    });
+  });
+
+  describe('create order', () => {
+    it('user data', () => {
+      cy.get('header').contains('Test').should('exist');
+    });
+
+    it('should be successfully creation', () => {
+      cy.get('[data-cy=bun-ingredients]').contains('Добавить').click();
+      cy.get('[data-cy=mains-ingredients]').contains('Добавить').click();
+      cy.get('[data-cy=sauces-ingredients]').contains('Добавить').click();
+      cy.get('[type=button]').contains('Оформить заказ').click();
+
+      cy.wait('@makeOrder', { timeout: 1000 })
+        .its('response.statusCode')
+        .should('eq', 200);
+
+      cy.get('[id=modals]').contains('123').should('be.visible');
+      cy.get('[id=modals]').find('button').click().should('not.exist');
+
+      cy.get('[id=root]')
+        .should('contain', 'Выберите булки')
+        .and('contain', 'Выберите начинку');
     });
   });
 });
